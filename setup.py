4d709ccbb69d057cc4d59240cd04b7aa573a2c21
unknown <macha.mounica@gmail.com>
2026-02-06 11:30:45 +0530

    requriments added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="mounika.macha",
+    author_email="macha.mounica@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
