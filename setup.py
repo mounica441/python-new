4476160b6f7519ac871748190f132d5db24fe8ce
mounika.macha <53563733+mounica441@users.noreply.github.com>
2026-02-09 11:00:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="medical_chatbot",
     version="0.1.0",
-    author="mounika.macha",
-    author_email="macha.mounica@gmail.com",
+    author="************(user name git hub)",
+    author_email="***********@gmail.com(email git hub)",
     packages=find_packages(),
     install_requires=[]
 )
